b7398d13767f4a895b737daa02b623b7ede83a06
Orell Bühler <me@orellbuehler.ch>
2026-02-26 00:00:20 +0100

    fix: use dynamic env import for sentry DSN in client hooks

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,9 @@
 import * as Sentry from '@sentry/sveltekit';
-import { PUBLIC_SENTRY_DSN } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
-if (PUBLIC_SENTRY_DSN) {
+if (env.PUBLIC_SENTRY_DSN) {
 	Sentry.init({
-		dsn: PUBLIC_SENTRY_DSN,
+		dsn: env.PUBLIC_SENTRY_DSN,
 		tracesSampleRate: 1.0,
 		replaysSessionSampleRate: 0,
 		replaysOnErrorSampleRate: 1.0,
